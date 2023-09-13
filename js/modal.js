64cf547a6e11d29151567623b22e5eeb03aa0c9c
Pavlo1508 <130464415+Pavlo1508@users.noreply.github.com>
2023-09-13 14:53:37 +0200

    .

--- js/modal.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("modal-open-btn").addEventListener("click", function () {
-	document.getElementById("modal-order").classList.add("is-open")
-})
-
-document.getElementById("modal-close-order-btn").addEventListener("click", function () {
-	document.getElementById("modal-order").classList.remove("is-open")
-})
